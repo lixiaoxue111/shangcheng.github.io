d58f2ca2a8cb167317d71a47e1f221d7b92e3ee0
newlls <523786175@qq.com>
2018-10-24 15:29:20 +0800

    shangcheng

--- js/main.js
+++ js/main.js
@@ -1,27 +1,4 @@
 
-/* 换肤开始*/
-$('.skin li').on('click',function () {
-    $(this).addClass('select').siblings().removeClass('select');
-    $('.nav').attr('class','nav skin'+$(this).index());
-    setCookie('skin',$(this).index(),0);
-});
-var index=getCookie('skin');
-$('.skin li').eq(index).trigger('click');
-/* 换肤结束*/
-/*导航效果 开始*/
-$(function () {
-   $('.nav li').hover(function () {
-       $(this).find('.jnNav').show();
-   },function () {
-      $(this).find('.jnNav').hide();
-   })
-});
-/*导航效果 结束*/
-/* hot开始*/
-$(function () {
-    $('.cata-info .promoted').append('<s class="hot"></s>')
-});
-/* hot结束*/
 /* 轮播图开始 */
 var count=0;
 var timer;
